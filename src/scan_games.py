ecafdcd404cf539fdc28e924e17dc106b6bb440e
Tair Shechter <tair.shechter@overwolf.com>
2025-08-21 12:12:32 +0300

    case sensitive exe check, erro handling and enumerator

--- src/scan_games.py
+++ src/scan_games.py
@@ -13,18 +13,21 @@ def get_exe_files_recursive(src_path):
     results = []
 
     if os.path.isfile(src_path):
-        if src_path.endswith(".exe"):
+        if src_path.lower().endswith(".exe"):
             results.append(src_path)
     else:
-        for item in os.listdir(src_path):
-            src_item = os.path.join(src_path, item)
-            results.extend(get_exe_files_recursive(src_item))
+        try:
+            for item in os.listdir(src_path):
+                src_item = os.path.join(src_path, item)
+                results.extend(get_exe_files_recursive(src_item))
+        except PermissionError:
+            pass
+        except Exception as e:
+            print(f"Error: {e}")
 
     return results
 
 def print_games_file_names(paths_list):
-    index = 1
-    for path in paths_list:       
+    for index, path in enumerate(paths_list, start=1):       
         game_file_name = os.path.basename(path)
         print(f"{index}. {game_file_name}")
-        index += 1
